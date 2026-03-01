0917740368a5ab7e02528b8aa66fe3ae69c4f5ca
Ampte B Marak <amptebmarak@gmail.com>
2026-03-01 15:10:43 +0530

    Modified index.js

--- index.js
+++ index.js
@@ -5,12 +5,18 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
-
-
-app.use(cors());
+/* middleware */
 app.use(express.json());
 
+/* CORS (IMPORTANT) */
+app.use(cors({
+  origin: [
+    "https://garo2.com",
+    "https://www.garo2.com"
+  ]
+}));
 
+/* API routes */
 app.use("/api/users", require("./routes/users"));
 app.use("/api/dictionary", require("./routes/dictionary"));
 app.use("/api/lessons", require("./routes/lessons"));
@@ -18,12 +24,10 @@ app.use("/api/stats", require("./routes/stats"));
 app.use("/api/translate", require("./routes/translate"));
 app.use("/api/chatbot", require("./routes/chatbot"));
 
-
-
 app.get("/", (req, res) => {
-  res.send('<h1>Welcome to the Server site of Garo2</h1>');
+  res.send("Garo2 Backend API Running");
 });
 
 app.listen(port, () => {
-  console.log("Server running on", port);
+  console.log(`Server running on ${port}`);
 });
