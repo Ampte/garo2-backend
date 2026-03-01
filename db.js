3667da9655e940c68a09244b3bf694c72135969a
Ampte B Marak <amptebmarak@gmail.com>
2026-03-01 15:17:51 +0530

    Add mysql2

--- db.js
+++ db.js
@@ -1,11 +1,14 @@
-const mysql = require("mysql");
+require("dotenv").config();
+const mysql = require("mysql2");
 
-
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+const db = mysql.createPool({
+  host: process.env.DB_HOST,      // usually localhost
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-module.exports = db;
+module.exports = db.promise();
