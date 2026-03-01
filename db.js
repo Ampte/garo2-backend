bce0f064c017802b2fcefe1bdfa4d02853d833ca
Ampte B Marak <amptebmarak@gmail.com>
2026-03-01 16:45:37 +0530

    Change db.js

--- db.js
+++ db.js
@@ -1,14 +1,12 @@
-require("dotenv").config();
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,      // usually localhost
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  connectionLimit: 10
 });
 
-module.exports = db.promise();
+module.exports = db;
