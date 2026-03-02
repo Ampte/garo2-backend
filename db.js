1b0e4d3f8cb9d7553deb30c1f69d1aa82693adbc
Ampte B Marak <amptebmarak@gmail.com>
2026-03-03 01:26:30 +0530

    Renew everythin

--- db.js
+++ db.js
@@ -1,16 +1,27 @@
 require("dotenv").config();
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: 3306,
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-db.connect((error) => {
-  if(error) throw error;
-  console.log("Connected to db");
+// test connection safely
+db.getConnection((err, connection) => {
+  if (err) {
+    console.error(" DB Connection Error:", err.message);
+    return;
+  }
+
+  console.log("MySQL Pool Connected");
+  connection.release();
 });
 
 module.exports = db;
